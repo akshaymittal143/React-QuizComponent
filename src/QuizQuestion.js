33bf6d9effecc05dbefb4451bb7e460d2f41a018
Akshay Mittal <akshay.mittal@coxautoinc.com>
2020-06-24 11:56:14 -0500

    m6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,15 +3,27 @@ import PropTypes from "prop-types";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 export default class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState((state) => {
+        return { incorrectAnswer: !state.incorrectAnswer };
+      });
     }
   }
 
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
